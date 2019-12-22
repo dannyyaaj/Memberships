a9d3a361e60939dc28c353e68af1fc4ba5a49c3f
Danny Yang <dannyyaaj@gmail.com>
2019-12-22 00:52:39 -0600

    Alterig product details view

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -38,7 +38,10 @@ namespace Memberships.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
-            return View(product);
+
+            var model = await product.Convert(db);
+
+            return View(model);
         }
 
         // GET: Admin/Product/Create
@@ -118,6 +121,7 @@ namespace Memberships.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
+
             return View(product);
         }
 
