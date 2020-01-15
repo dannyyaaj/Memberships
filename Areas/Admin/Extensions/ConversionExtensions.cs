5b98182708793977218a98d2214369803dd3c8ee
Danny Yang <dannyyaaj@gmail.com>
2020-01-15 00:29:02 -0600

    Alter extension methods for SubscriptonProductModel

--- Areas/Admin/Extensions/ConversionExtensions.cs
+++ Areas/Admin/Extensions/ConversionExtensions.cs
@@ -14,6 +14,7 @@ namespace Memberships.Areas.Admin.Extensions
     //Needs to be public static because it is an extension method
     public static class ConversionExtensions
     {
+        #region Product
         //return a collection of ProductModels
         public static async Task<IEnumerable<ProductModel>> Convert(
             this IEnumerable<Product> products, ApplicationDbContext db)
@@ -63,7 +64,9 @@ namespace Memberships.Areas.Admin.Extensions
 
             return model;
         }
+        #endregion
 
+        #region Product Item
         public static async Task<IEnumerable<ProductItemModel>> Convert(
           this IQueryable<ProductItem> productItems, ApplicationDbContext db)
         {
@@ -145,5 +148,90 @@ namespace Memberships.Areas.Admin.Extensions
                 }
             }
         }
+        #endregion
+
+        #region Subscription Product
+        public static async Task<IEnumerable<SubscriptionProductModel>> Convert(
+          this IQueryable<SubscriptionProduct> subscriptionProduct, ApplicationDbContext db)
+        {
+            if (subscriptionProduct.Count().Equals(0))
+                return new List<SubscriptionProductModel>();
+
+            return await (from sp in subscriptionProduct
+                          select new SubscriptionProductModel
+                          {
+                              SubscriptionId = sp.SubscriptionId,
+                              ProductId = sp.ProductId,
+                              SubscriptionTitle = db.Subscriptions.FirstOrDefault(
+                                  i => i.Id.Equals(sp.SubscriptionId)).Title,
+                              ProductTitle = db.Products.FirstOrDefault(
+                                  p => p.Id.Equals(sp.ProductId)).Title
+                          }).ToListAsync();
+        }
+
+        public static async Task<SubscriptionProductModel> Convert(
+        this SubscriptionProduct subscriptionProduct, ApplicationDbContext db, bool addListData = true)
+        {
+            var model = new SubscriptionProductModel
+            {
+                SubscriptionId = subscriptionProduct.SubscriptionId,
+                ProductId = subscriptionProduct.ProductId,
+                Subscriptions = addListData ? await db.Subscriptions.ToListAsync() : null,
+                Products = addListData ? await db.Products.ToListAsync() : null,
+                SubscriptionTitle = (await db.Subscriptions.FirstOrDefaultAsync(i => i.Id.Equals(subscriptionProduct.SubscriptionId))).Title,
+                ProductTitle = (await db.Products.FirstOrDefaultAsync(p => p.Id.Equals(subscriptionProduct.ProductId))).Title
+            };
+
+            return model;
+        }
+
+        public static async Task<bool> CanChange(
+            this SubscriptionProduct subscriptionProduct, ApplicationDbContext db)
+        {
+            var oldPI = await db.SubscriptionProducts.CountAsync(sp =>
+                sp.ProductId.Equals(subscriptionProduct.OldProductId) &&
+                sp.SubscriptionId.Equals(subscriptionProduct.OldSubscriptionId));
+
+            var newPI = await db.SubscriptionProducts.CountAsync(sp =>
+                sp.ProductId.Equals(subscriptionProduct.ProductId) &&
+                sp.SubscriptionId.Equals(subscriptionProduct.SubscriptionId));
+
+            return oldPI.Equals(1) && newPI.Equals(0);
+        }
+
+        public static async Task Change(this SubscriptionProduct subscriptionProduct, ApplicationDbContext db)
+        {
+            var oldSubscriptionProduct = await db.SubscriptionProducts.FirstOrDefaultAsync(
+                sp => sp.ProductId.Equals(subscriptionProduct.OldProductId) &&
+                sp.SubscriptionId.Equals(subscriptionProduct.OldSubscriptionId));
+
+            var newSubscriptionProduct = await db.SubscriptionProducts.FirstOrDefaultAsync(
+                sp => sp.ProductId.Equals(subscriptionProduct.ProductId) &&
+                sp.SubscriptionId.Equals(subscriptionProduct.SubscriptionId));
+
+            if (oldSubscriptionProduct != null && newSubscriptionProduct == null)
+            {
+                newSubscriptionProduct = new SubscriptionProduct
+                {
+                    SubscriptionId = subscriptionProduct.SubscriptionId,
+                    ProductId = subscriptionProduct.ProductId
+                };
+
+                using (var transaction = new TransactionScope(
+                    TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    try
+                    {
+                        db.SubscriptionProducts.Remove(oldSubscriptionProduct);
+                        db.SubscriptionProducts.Add(newSubscriptionProduct);
+
+                        await db.SaveChangesAsync();
+                        transaction.Complete();
+                    }
+                    catch { transaction.Dispose(); }
+                }
+            }
+        }
+        #endregion
     }
 }
