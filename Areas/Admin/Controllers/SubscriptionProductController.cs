662e60c52c80aa83e670da76fb1d1e274f3522bb
Danny Yang <dannyyaaj@gmail.com>
2020-01-20 14:49:45 -0600

    Add GetSubscriptonProduct method to SubscriptionProduct controller

--- Areas/Admin/Controllers/SubscriptionProductController.cs
+++ Areas/Admin/Controllers/SubscriptionProductController.cs
@@ -117,6 +117,21 @@ namespace Memberships.Areas.Admin.Controllers
             return RedirectToAction("Index");
         }
 
+        private async Task<SubscriptionProduct> GetSubscriptionProduct(int? subscriptionId, int? productId)
+        {
+            try
+            {
+                int subscId = 0, prdId = 0;
+                int.TryParse(subscId.ToString(), out subscId);
+                int.TryParse(productId.ToString(), out prdId);
+                var subscriptionProduct = await db.SubscriptionProducts.FirstOrDefaultAsync(
+                        sp => sp.ProductId.Equals(prdId) && sp.SubscriptionId.Equals(subscId)
+                    );
+                return subscriptionProduct;
+            }
+            catch { return null; }
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
