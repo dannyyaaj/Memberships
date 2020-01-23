3e76046134a3080cb4a8ac22fac21f70fd37fbf9
Danny Yang <dannyyaaj@gmail.com>
2020-01-22 22:44:15 -0600

    Add and style _ThumbnailPartialView

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -30,7 +30,8 @@ namespace Memberships
                       "~/Scripts/AdminMenu.js"));
 
             bundles.Add(new StyleBundle("~/Content/membership").Include(
-                      "~/Content/navbar.css"));
+                      "~/Content/navbar.css",
+                       "~/Content/thumbnails.css"));
         }
     }
 }
