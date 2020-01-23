54811eedf3cfad686916b98ecd2847e079a3e886
Danny Yang <dannyyaaj@gmail.com>
2020-01-22 22:12:24 -0600

    Use vanilla javascript instead of jquery to handle dropdown menu logic

--- Scripts/AdminMenu.js
+++ Scripts/AdminMenu.js
@@ -1,7 +1,12 @@
-﻿$(function () {
-    //target li element with attribute of data-admin-menu
-    $('[data-admin-menu]').hover(function () {
-        console.log($(this), 'THIS');
-        $(this).toggleClass('open');
-    });
-});
+﻿window.onload = function() {
+
+    let dropdown = document.querySelector('li.dropdown');
+
+    dropdown.onmouseover = function () {
+        dropdown.classList.toggle('open');
+    }
+
+    dropdown.onmouseout = function () {
+        dropdown.classList.toggle('open');
+    }
+};
